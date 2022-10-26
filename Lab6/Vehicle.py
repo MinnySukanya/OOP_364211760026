e33905739a07ce6113c12ec3ec777c20fd371936
Windows 11 <sukanya.khu@rmutavmail.com>
2022-10-26 13:30:58 +0700

    Lab0

---
+++ Lab6/Vehicle.py
@@ -0,0 +1,25 @@
+"""
+Name: Sukanya Khunthana
+TD: SID: {364211760026}
+Group: {MIT221}
+"""
+#from lab_6.vihecle_append import brand
+
+
+class Vehicle:
+    #class attribute
+    my_Vehicle = []
+def __init__(self,brand,model,color,maxspeed,price):
+    self.brand = brand
+    self.model = model
+    self.color = color
+    self.maxspeed = maxspeed
+    self.price = price
+    self.my_vihecle_append(self)
+
+    def Vehicle_detail(self):
+        print(f'Brand:{self.brand} '
+              f'Model:{self.model} '
+              f'color:{self.color} '
+              f'maxspeed:{self.maxspeed} '
+              f'price:{self.price}')
